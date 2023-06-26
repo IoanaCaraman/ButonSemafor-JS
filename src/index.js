1d4ef97bebd62aff7b9fd8597a7935b59fc0df88
Ioana Caraman <ioanacaraman3@gmail.com>
2023-06-26 21:37:30 +0300

    implementare in JavaScript

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 function changeButtonColor() {
     var button = document.getElementById("coloredButton");
 
-    if(button.classList.contains("btn-danger")){
+    if(button.classList.contains("btn-danger")) {
         button.classList.remove("btn-danger");
         button.classList.add("btn-warning");
     }else if (button.classList.contains("btn-warning")) {
         button.classList.remove("btn-warning");
         button.classList.add("btn-success");
-    } else if (button.classList.contains("btn-success")){
+    }else if (button.classList.contains("btn-success")) {
         button.classList.remove("btn-success");
         button.classList.add("btn-danger");
     }
@@ -16,6 +16,7 @@ function changeButtonColor() {
 function timeColor() {
     setInterval(changeButtonColor, 10000);
 }
+
 window.addEventListener("load", (event) => {
     timeColor();
 });
