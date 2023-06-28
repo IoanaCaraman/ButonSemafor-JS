5fb62ac308f6a7df6935513c595b316377c1ecba
Ioana Caraman <ioanacaraman3@gmail.com>
2023-06-28 13:01:38 +0300

    ButonSemafor-JS after codereview

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 function changeButtonColor() {
-    var button = document.getElementById("coloredButton");
+    let button = document.getElementById("coloredButton");
 
-    if(button.classList.contains("btn-danger")) {
+    if (button.classList.contains("btn-danger")) {
         button.classList.remove("btn-danger");
         button.classList.add("btn-warning");
-    }else if (button.classList.contains("btn-warning")) {
+    } else if (button.classList.contains("btn-warning")) {
         button.classList.remove("btn-warning");
         button.classList.add("btn-success");
-    }else if (button.classList.contains("btn-success")) {
+    } else if (button.classList.contains("btn-success")) {
         button.classList.remove("btn-success");
         button.classList.add("btn-danger");
     }
